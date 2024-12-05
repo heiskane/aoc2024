adb3dd04637e336ed8add0a9b52a02e7e5869e08
Niko Heiskanen <henipe@gmail.com>
2024-12-05 18:46:18 +0200

    (day4): part2

--- day4/main.exs
+++ day4/main.exs
@@ -24,6 +24,25 @@ defmodule Solver do
       [ diag | acc ]
     end)
   end
+
+  def part2(matrix) do
+    Enum.reduce(1..length(matrix) - 2, 0, fn i, acc ->
+      acc + Enum.reduce(1..length(Enum.at(matrix, i)) - 2, 0, fn j, acc ->
+        tl = Enum.at(matrix, i - 1) |> Enum.at(j - 1)
+        tr = Enum.at(matrix, i - 1) |> Enum.at(j + 1)
+        mid = Enum.at(matrix, i) |> Enum.at(j)
+        bl = Enum.at(matrix, i + 1) |> Enum.at(j - 1)
+        br = Enum.at(matrix, i + 1) |> Enum.at(j + 1)
+
+        a1 = Enum.join([tl, mid, br])
+        a2 = Enum.join([bl, mid, tr])
+
+        if (a1 === "MAS" || a1 === "SAM") && (a2 === "MAS" || a2 === "SAM") do
+          acc + 1
+        else acc end
+      end)
+    end)
+  end
 end
 
 {:ok, input1} = File.read("input_1b.txt")
@@ -43,4 +62,10 @@ end).()
   (length(String.split(&1, "SAMX")) - 1)
 ))
 |> Enum.sum()
+|> IO.inspect()
+
+input1
+|> String.split("\n", trim: true)
+|> Enum.map(&String.graphemes(&1))
+|> Solver.part2()
 |> IO.inspect()
