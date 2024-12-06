12422c3e36214686ef9eab85d5e5a729f22c92b2
Niko Heiskanen <henipe@gmail.com>
2024-12-06 09:34:30 +0200

    (day5): cleanup

--- day5/main.exs
+++ day5/main.exs
@@ -1,5 +1,27 @@
 {:ok, input1} = File.read("input_1b.txt")
 
+defmodule Solver do
+
+  def check_validity(rules, update) do
+    Enum.reduce(rules, true, fn y, acc ->
+      [left, right] = String.split(y, "|")
+      left_i = Enum.find_index(update, &(&1 === left))
+      right_i = Enum.find_index(update, &(&1 === right))
+
+      if (!is_nil(left_i) && !is_nil(right_i)) &&
+         (left_i > right_i) do false else acc
+      end
+    end)
+  end
+
+  def get_mid(update) do
+    mid = Integer.floor_div(length(update), 2)
+    Enum.at(update, mid)
+    |> String.to_integer()
+  end
+
+end
+
 [rules, updates] = input1
 |> String.split("\n\n")
 |> (fn [rules, updates] -> [String.split(rules), String.split(updates)] end).()
@@ -7,52 +29,26 @@
 updates
 |> Enum.map(&String.split(&1, ","))
 |> Enum.reduce(0, fn x, acc ->
-  # IO.inspect(x)
-  valid = Enum.reduce(rules, true, fn y, acc ->
-    [left, right] = String.split(y, "|")
-    left_i = Enum.find_index(x, &(&1 === left))
-    right_i = Enum.find_index(x, &(&1 === right))
-    # IO.inspect({left, left_i, right, right_i})
-
-    if (!is_nil(left_i) && !is_nil(right_i)) &&
-       (left_i > right_i) do false else acc
-    end
-  end)
-  # IO.inspect(valid)
-  if valid do
-    mid = Integer.floor_div(length(x), 2)
-    Enum.at(x, mid)
-    |> String.to_integer()
-    |> Kernel.+(acc)
-  else acc end
+  case Solver.check_validity(rules, x) do
+    true -> Solver.get_mid(x) + acc
+    false -> acc
+  end
 end)
 |> IO.inspect()
 
 updates
 |> Enum.map(&String.split(&1, ","))
 |> Enum.reduce(0, fn x, acc ->
-  valid = Enum.reduce(rules, true, fn y, acc ->
-    [left, right] = String.split(y, "|")
-    left_i = Enum.find_index(x, &(&1 === left))
-    right_i = Enum.find_index(x, &(&1 === right))
-
-    if (!is_nil(left_i) && !is_nil(right_i)) &&
-       (left_i > right_i) do false else acc
-    end
-  end)
-
-  if valid do acc else
-    sorted = Enum.sort(x, fn a, b ->
-      [left, right] = Enum.find(rules, &(String.contains?(&1, a) && String.contains?(&1, b)))
-      |> String.split("|")
-      (a === left && b === right)
-    end)
-
-    mid = Integer.floor_div(length(sorted), 2)
-
-    Enum.at(sorted, mid)
-    |> String.to_integer()
-    |> Kernel.+(acc)
+  case Solver.check_validity(rules, x) do
+    false ->
+      Enum.sort(x, fn a, b ->
+        [left, right] = Enum.find(rules, &(String.contains?(&1, a) && String.contains?(&1, b)))
+        |> String.split("|")
+        (a === left && b === right)
+      end)
+      |> Solver.get_mid()
+      |> Kernel.+(acc)
+    true -> acc
   end
 end)
 |> IO.inspect()
