3b5ab0f83d1bb112b63036f5503eb8dd55d07987
Niko Heiskanen <henipe@gmail.com>
2024-12-07 12:05:49 +0200

    (day7): add async for more speed

--- day7/main.exs
+++ day7/main.exs
@@ -1,9 +1,6 @@
 {:ok, input} = File.read("input_2.txt")
 
-defmodule Combinations do
-  # chatgpt code (modified)
-  # TODO: make something nicer i guess
-
+defmodule Solver do
   def generate_combinations(chars, length, acc \\ [])
   def generate_combinations(_chars, 0, acc), do: [Enum.reverse(acc)]
   def generate_combinations(chars, length, acc) do
@@ -11,25 +8,25 @@ defmodule Combinations do
       generate_combinations(chars, length - 1, [char | acc])
     end)
   end
-end
 
-defmodule Solver do
   def is_valid(row, funcs) do
     target = hd(row)
     values = tl(row)
-    operations = Combinations.generate_combinations(funcs, (length(values) - 1))
+    operations = Solver.generate_combinations(funcs, (length(values) - 1))
 
-    Enum.reduce_while(operations, false, fn ops, found ->
+    valid = Enum.reduce_while(operations, false, fn ops, found ->
       result = Enum.with_index(tl(values))
       |> Enum.reduce(hd(values), fn {value, i}, acc ->
         Enum.at(ops, i)  # get operator
         |> (& &1.(acc, value)).()  # this feels naughty
       end)
       cond do
-        result === target -> {:halt, true}
+        result === target -> {:halt, true}  # stop on first valid result
         result !== target -> {:cont, found}
       end
     end)
+
+    if valid do target else 0 end
   end
 end
 
@@ -44,21 +41,23 @@ end)
 # part1
 calibrations
 |> Enum.map(fn row ->
-  case Solver.is_valid(row, [&Kernel.+/2, &Kernel.*/2]) do
-    true -> hd(row)
-    false -> 0
-  end
+  Task.async(fn ->
+    Solver.is_valid(row, [&Kernel.+/2, &Kernel.*/2])
+  end)
 end)
+|> Enum.map(&Task.await(&1))
 |> Enum.sum()
 |> IO.inspect()
 
 # part2
 calibrations
 |> Enum.map(fn row ->
-  case Solver.is_valid(row, [&Kernel.+/2, &Kernel.*/2, &(String.to_integer("#{&1}#{&2}"))]) do
-    true -> hd(row)
-    false -> 0
-  end
+  Task.async(fn ->
+    Solver.is_valid(
+      row, [&Kernel.+/2, &Kernel.*/2, &(String.to_integer("#{&1}#{&2}"))]
+    )
+  end)
 end)
+|> Enum.map(&Task.await(&1))
 |> Enum.sum()
 |> IO.inspect()
