d76548d1839dba6b97965225dda57a2c32e20c30
Niko Heiskanen <henipe@gmail.com>
2024-12-11 18:02:47 +0200

    (day11): part1 much faster

--- day11/main.exs
+++ day11/main.exs
@@ -1,29 +1,39 @@
 {:ok, input} = File.read("input_2.txt")
 
 defmodule Solver do
+
+  def num_len(num) when num === 0 do 1 end
+  def num_len(num) do trunc(:math.floor(:math.log10(abs(num))) + 1) end
+
+  # https://stackoverflow.com/a/32016977
+  def split_stone(stone) do
+    len = Solver.num_len(stone)
+    left = div(stone, trunc(:math.pow(10, div(len, 2))))
+    right = trunc(stone - left * :math.pow(10, div(len, 2)))
+    {left, right}
+  end
+
   def blink(stones) do
     Enum.reduce(stones, [], fn stone, acc ->
-      if stone === "0" do
-        acc ++ ["1"]
+      if stone === 1 do
+        [ 1 | acc ]
       else
-        if rem(String.length(stone), 2) === 0 do
-          {left, right} = String.split_at(stone, div(String.length(stone), 2))
-
-          # This spaghetti is to trim leading zeroes
-          left = String.to_integer(left)
-          right = String.to_integer(right)
-
-          acc ++ [Integer.to_string(left), Integer.to_string(right)]
+        if rem(Solver.num_len(stone), 2) === 0 do
+          {left, right} = Solver.split_stone(stone)
+          [ left, right | acc ]
         else
-          num = String.to_integer(stone)
-          acc ++ [Integer.to_string(num * 2024)]
+          [ stone * 2024 | acc ]
         end
       end
     end)
   end
 
   def solve(stones, target_count, count \\ 0) do
+    IO.inspect(length(stones), label: "stone count")
+    IO.inspect(count, label: "blinking count")
     if count < target_count do
+      # TODO: can we chunk the massive list into something more manageable
+      # TODO: try using maps or something
       Solver.blink(stones)
       # |> tap(&(IO.inspect(&1, pretty: false, limit: :infinity)))
       |> Solver.solve(target_count, count + 1)
@@ -31,11 +41,25 @@ defmodule Solver do
   end
 end
 
+{time, result} = :timer.tc(fn ->
 input
-|> String.trim("\n")
-|> String.split(" ")
-# |> Solver.blink()
-|> Solver.solve(25)
-# |> Solver.solve(75)
-|> length()
-|> IO.inspect()
+  |> String.trim("\n")
+  |> String.split(" ")
+  |> Enum.map(&String.to_integer/1)
+  |> Solver.solve(25)
+  |> length()
+  |> IO.inspect()
+end)
+
+IO.inspect("#{result} (#{time / 1_000}ms)", label: "Part 1")
+
+# {time, result} = :timer.tc(fn ->
+#   input
+#   |> String.trim("\n")
+#   |> String.split(" ")
+#   |> Enum.map(&String.to_integer/1)
+#   |> Solver.solve(75)
+#   |> length()
+# end)
+#
+# IO.inspect("#{result} (#{time / 1_000}ms)", label: "Part 2")
